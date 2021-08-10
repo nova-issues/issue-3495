ba2bac681efdcf62f588631ce48c6ec1176c04e4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-08-11 07:57:28 +0800

    Add reproducing code for laravel/nova-issues#3495
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ app/Nova/Actions/CreateProducts.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\DestructiveAction;
+use Laravel\Nova\Fields\ActionFields;
+
+class CreateProducts extends DestructiveAction implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        $models->each(function ($model) {
+            ray("Running CreateProducts on [{$model->id}]");
+        });
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+}
